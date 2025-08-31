cb6b00be85ecb4c58497e7d7fbbb3cd055c460bb
Auxiguilar <filip.myras@gmail.com>
2025-08-31 10:21:44 +0200

    made it usable

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from stats import count_book_words, count_book_characters, make_character_list, sort_on
+import sys
 
-def get_book_text(book_path):
-    with open(book_path) as b:  # mysterious automatic opening and closing of file
+def get_book_text(path):
+    with open(path) as b:  # mysterious automatic opening and closing of file
         text = b.read()
         return text
 
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    word_count = count_book_words(book_text)
-    character_count = count_book_characters(book_text)
+def main(path):
+    text = get_book_text(path)
+    word_count = count_book_words(text)
+    character_count = count_book_characters(text)
     character_list = make_character_list(character_count)
     character_list.sort(reverse=True, key=sort_on)
 
@@ -24,5 +25,9 @@ def main():
     print("============= END ===============")
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        main(sys.argv[1])
 
