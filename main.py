bc2ae67bddd53c1780235a4842cb07f017e1196a
Auxiguilar <filip.myras@gmail.com>
2025-08-31 09:42:04 +0200

    appeased boots

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_book_words, count_book_letters, make_letter_list, sort_on
+from stats import count_book_words, count_book_characters, make_character_list, sort_on
 
 def get_book_text(book_path):
     with open(book_path) as b:  # mysterious automatic opening and closing of file
@@ -8,23 +8,21 @@ def get_book_text(book_path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = count_book_words(book_text)
-    letter_count = count_book_letters(book_text)
-    letter_list = make_letter_list(letter_count)
-    letter_list.sort(reverse=True, key=sort_on)
+    character_count = count_book_characters(book_text)
+    character_list = make_character_list(character_count)
+    character_list.sort(reverse=True, key=sort_on)
 
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    
-    for letter in letter_list:
-        if letter["letter"].isalpha() == False:
-            pass
-        else:
-            print(f"{letter["letter"]}: {letter["num"]}")
+
+    for char in character_list:
+        print(f"{char["char"]}: {char["num"]}")
     
     print("============= END ===============")
-    
-main()
+
+if __name__ == "__main__":
+    main()
 
--- stats.py
+++ stats.py
@@ -2,26 +2,26 @@ def count_book_words(text):
     return len(text.split())
 
 
-def count_book_letters(text):
-    letter_count = {}
+def count_book_characters(text):
+    character_count = {}
 
-    for t in text.lower():
-        if t not in letter_count:
-            letter_count[t] = 1
-        else:
-            letter_count[t] += 1
-
-    return letter_count
-
-
-def make_letter_list(letter_count): # just makes appropriate list out of letter_count
-    letter_list = []
-
-    for letter in letter_count:
-        letter_list.append({"letter": letter, "num": letter_count[letter]})
-
-    return letter_list
+    for char in text.lower():
+        if char.isalpha() == True and char not in character_count:  # now only alphabeticals
+            character_count[char] = 1
+        elif char.isalpha() == True:
+            character_count[char] += 1
+    
+    return character_count
 
 
-def sort_on(letter_list):
-    return letter_list["num"]
+def make_character_list(character_count): # just makes appropriate list out of letter_count
+    character_list = []
+
+    for char in character_count:
+        character_list.append({"char": char, "num": character_count[char]})
+
+    return character_list
+
+
+def sort_on(character_list):
+    return character_list["num"]
